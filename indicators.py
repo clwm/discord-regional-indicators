455b363bee4c621514180a42d15e092140cf8d0d
clwm <29554879+clwm@users.noreply.github.com>
2020-11-25 11:36:11 +1100

    Code cleanup

--- indicators.py
+++ indicators.py
@@ -1,6 +1,16 @@
-i = input("String to convert: ")
+i = input("Input: ")
 
-d = { '0' : 'zero', '1' : 'one', '2' : 'two', '3' : 'three', '4' : 'four', '5' : 'five', '6' : 'six', '7' : 'seven', '8' : 'eight', '9' : 'nine' }
+d = { '0' : 'zero',
+      '1' : 'one',
+      '2' : 'two',
+      '3' : 'three',
+      '4' : 'four',
+      '5' : 'five',
+      '6' : 'six',
+      '7' : 'seven',
+      '8' : 'eight',
+      '9' : 'nine'
+      }
 
 for c in i:
     if c.isnumeric():
